7e7e4503dad34649af15dd996ff5f5838c2a0ff2
do_aki <do.hiroaki@gmail.com>
2012-01-21 17:33:13 +0900

    fix php-src path

--- run_test.php
+++ run_test.php
@@ -3,7 +3,7 @@
 require_once dirname(__FILE__) . '/lime.php';
 
 $h = new lime_harness(array(
-  'php_cli' => dirname(__FILE__) . '/php-5.4.0alpha3/sapi/cli/php',
+  'php_cli' => dirname(__FILE__) . '/php-src/sapi/cli/php',
 ));
 $h->register('test');
 $h->run();
